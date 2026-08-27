ec00b23a872f231d8fcba67f4635a4484bc99329
Pawel Koziol <pp.koziol@gmail.com>
2026-08-27 12:30:20 +0200

    new commandline arguments

--- main.go
+++ main.go
@@ -44,7 +44,7 @@ import (
 	"strconv"
 )
 
-const versionString = "1.1.01"
+const versionString = "1.1.03"
 
 // init() is guaranteed to run before main()
 func init() {
@@ -83,14 +83,14 @@ func main() {
 	if len(os.Args) > 1 {
 		switch os.Args[1] {
 		case "tune":
-			tv2Tune("quiet-labeled.epd", 5000, 1.0)
+			ctTune("d:/epd/lichess-quiet.epd", 200, 2.0)
 			return
 		case "tunefile":
 			if len(os.Args) < 3 {
 				fmt.Println("usage: rodent-v tunefile <epd-or-book-file>")
 				return
 			}
-			tv2Tune(os.Args[2], 5000, 1.0)
+			ctTune(os.Args[2], 5000, 1.0)
 			return
 		case "datagen":
 			if len(os.Args) < 5 {
@@ -115,6 +115,19 @@ func main() {
 			err := filterQuietBulletFile(os.Args[2], os.Args[3])
 			if err != nil {
 				fmt.Println("Error:", err)
+			}
+			return
+		case "fit":
+			ss := new(SearchState)
+			ss.tt = &mainTT
+			ss.isMainThread = true
+			ss.isUsingNNUE = false
+
+			fit, err := texelFitFile("d:/epd/zuri_orig.epd", func(p *Pos) int {
+				return eval_internal(p, false, ss)
+			})
+			if err == nil {
+				fmt.Println(fit)
 			}
 			return
 		case "alloptions":
